69562d9011420cc59541a9e5b2052f7497d80753
Dan Perez <dan_rudolf_perez@yahoo.com>
2024-12-14 17:25:01 +0800

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,8 +26,8 @@ export default function Home() {
     sendGAEventCustom({ 
       action: 'click', 
       category: 'Button',
-      label: 'Watch how Pitaku works - Hero',
-      value: 'Watch how Pitaku works - Hero' 
+      label: 'Watch how Pitaku works',
+      value: 'Watch how Pitaku works' 
     })    
   }
 
